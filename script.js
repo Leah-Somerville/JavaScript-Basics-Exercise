fd4115c5ca78697cc478cbf0abc0fa00181bdb4c
Leah Somerville <leah.somerville@stu.barren.kyschools.us>
2023-01-10 10:33:25 -0600

    Exercise Complete

---
+++ script.js
@@ -0,0 +1,49 @@
+// 1
+console.log(7);
+
+// 2
+// Numbers, Strings, Booleans, Null, Undefined
+
+// 3
+// + - / * % **
+
+// 4
+console.log(5%2);
+
+// 5
+console.log('I\'M MARY POPPINS, Y\'ALL! ');
+
+// 6
+console.log("Hawk" + "eye");
+
+// 7
+console.log("Leah Somerville".length);
+
+// 8
+console.log("Avengers: Endgame"[16]);
+
+// BONUS
+// 1
+console.log("\\");
+
+// 2
+console.log((5**4) - 9);
+
+// 3
+
+
+// 4
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("OKOYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+
+// 5
+m = 25
+c = 4
+u = m+c
+console.log(u);
